3e4d231bfe67c7dd5f18cd42110b51a336d2594e
Kyle_Lucian <westnick13th@mail.ru>
2018-12-04 00:35:15 +0700

    Solved: Day 2 Inventory Management System part two

--- day-02-inventory-management-system/src/main/java/ru/lian/ky/advent/inventory/OneLetterDiffApp.java
+++ day-02-inventory-management-system/src/main/java/ru/lian/ky/advent/inventory/OneLetterDiffApp.java
@@ -20,16 +20,58 @@ public class OneLetterDiffApp {
 
     public static void main(String[] args) {
         try {
-            List<String> inputStrings = getInputStrings();
-            
+
+            String result = getString(getInputStrings());
+            logger.info("Result is: {}", result.isEmpty() ? "error" : result);
+
         } catch (URISyntaxException | IOException e) {
             logger.error("One letter diff error: {}", e);
         }
     }
 
+    private static String getString(List<String> strings) {
+        for (int i = 0; i < strings.size(); i++) {
+            for (int j = 0; j < strings.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                int position = findSingleDiffCharacterPosition(strings.get(i), strings.get(j));
+                if (position != -1) {
+                    return new StringBuilder(strings.get(i))
+                            .deleteCharAt(position)
+                            .toString();
+                }
+            }
+        }
+        return "";
+    }
+
     private static List<String> getInputStrings() throws URISyntaxException, IOException {
         Path path = Paths.get(Objects.requireNonNull(INPUT_FILE_URL).toURI());
         return Files.readAllLines(path);
+    }
+
+    private static int findSingleDiffCharacterPosition(String a, String b) {
+        if (a.length() != b.length()) {
+            return -1;
+        }
+
+        char[] aCharArray = a.toCharArray();
+        char[] bCharArray = b.toCharArray();
+
+        int firstDiffPosition = -1;
+        for (int i = 0; i < aCharArray.length; i++) {
+            char aChar = aCharArray[i];
+            char bChar = bCharArray[i];
+
+            if (aChar != bChar && firstDiffPosition == -1) {
+                firstDiffPosition = i;
+            } else if (aChar != bChar) {
+                return -1;
+            }
+        }
+        return firstDiffPosition;
     }
 
 }
